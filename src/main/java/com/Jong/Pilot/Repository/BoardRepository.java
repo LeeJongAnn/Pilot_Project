cf9223fdd74102c45f6a416a5483cea9a5fb4552
dlwhddksiq <dlwhddksiq@naver.com>
2022-11-27 13:55:22 +0900

    페이지 화면 정리

--- src/main/java/com/Jong/Pilot/Repository/BoardRepository.java
+++ src/main/java/com/Jong/Pilot/Repository/BoardRepository.java
@@ -1,11 +1,12 @@
 package com.Jong.Pilot.Repository;
 
 import com.Jong.Pilot.Entity.Board;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BoardRepository extends CrudRepository<Board,Integer> {
+public interface BoardRepository extends JpaRepository<Board,Integer> {
 
 
 }
