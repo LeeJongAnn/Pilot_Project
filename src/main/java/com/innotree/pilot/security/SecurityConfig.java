d41e719f622a28ccf5cfa43b54d9ed906a081162
JongAnn <dlwhddksiq@naver.com>
2022-12-22 17:43:57 +0900

    DB 외부 접속 허용 및 댓글 관련 오류 수정하고 있음

--- src/main/java/com/innotree/pilot/security/SecurityConfig.java
+++ src/main/java/com/innotree/pilot/security/SecurityConfig.java
@@ -39,6 +39,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests()
+                .antMatchers("/delete-reply/**").hasAnyAuthority("Admin","Normal")
                 .antMatchers("/users/**").hasAuthority("Admin").anyRequest()
                 .authenticated()
                 .and()
